118c5d7c5e733c1292abf579c922a8929ba860c2
GingerBreadMan1 <122408023+GingerBreadMan1@users.noreply.github.com>
2023-02-16 16:12:59 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,13 @@
 def league_points(wins, draws, losses):
-    ...
-
-
-
-
+    
+    wins = wins * 3
+    
+    draws = draws * 1
+    
+    losses = losses * 0
+    
+    total = wins + draws + losses
+    return total
+    
+print(league_points(5, 0, 2))
     
